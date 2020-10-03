d3f5cbbfdd624f43d24eb6dfcedebde213a10484
Vlad Ardelean <vlad.george.ardelean@gmail.com>
2020-10-03 23:42:53 +0300

    Script worked, but it didn't print anything. Fixed that

--- batch-scripts/count_nums.py
+++ batch-scripts/count_nums.py
@@ -2,4 +2,7 @@ from pyspark import SparkContext
 
 sc = SparkContext(appName='VladsApp')
 
-sc.parallelize([1,2,3,4,2,3,4,3,4,4,5,5,5,5,5,5,5,5,5,5]).map(lambda a: (a, 1)).reduceByKey(lambda a, b: a + b).collect()
+print(
+        sc.parallelize([1,2,3,4,2,3,4,3,4,4,5,5,5,5,5,5,5,5,5,5]).map(lambda a: (a, 1)).reduceByKey(lambda a, b: a + b).collect()
+        )
+
